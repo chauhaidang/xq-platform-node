fe2b82a9efd205adac9fdb5a6a0dfe57202adcb0
chauhaidang1 <chauhaidang1@gmail.com>
2024-09-26 22:49:33 +0700

    update config

--- xq-platform-test/cucumber.js
+++ xq-platform-test/cucumber.js
@@ -1,8 +1,8 @@
 module.exports = {
-    default: {
-        parallel: 1,
-        format: ['html:cucumber-report.html', 'progress-bar', '@cucumber/pretty-formatter'],
-        paths: ['features/**/*.feature'],
-        import: ['step-definitions/**/*.js']
-    }
+  default: {
+    parallel: 1,
+    format: ['html:cucumber-report.html', 'progress-bar', '@cucumber/pretty-formatter'],
+    paths: ['features/**/*.feature'],
+    import: ['step-definitions/**/*.js']
+  }
 }
--- xq-platform-test/features/test.feature
+++ xq-platform-test/features/test.feature
@@ -1,6 +1,6 @@
 Feature: Greeting
 
   Scenario:
-    Given I
+    Given I have this
     When I do this
     Then I got
